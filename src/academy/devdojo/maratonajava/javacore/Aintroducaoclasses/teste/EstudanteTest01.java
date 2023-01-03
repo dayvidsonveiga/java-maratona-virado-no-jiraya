b017ec7141fd8714c36119863558453ad67bb7bd
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-01-02 23:00:14 -0300

    Introducao Classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+    }
+}
