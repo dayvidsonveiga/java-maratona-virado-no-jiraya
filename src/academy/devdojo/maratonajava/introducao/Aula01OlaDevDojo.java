105cf41c58accf6bec61f11d52c4fb3a181017a5
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-01-02 22:56:52 -0300

    Aula01OlaDevDojo

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
+
+public class Aula01OlaDevDojo {
+    // Isso é um comentário de uma linha
+
+    /*
+        Isso é um comentário
+        de multipllas
+        linhas
+     */
+
+    public static void main(String[] args) {
+        System.out.println("KA-ME-KA-ME-HAAAA");
+    }
+}
